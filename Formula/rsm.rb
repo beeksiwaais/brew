49f3e2ccaeea16618d0a652cbab367b0f90e76dc
Beeksi Waais <181389+beeksiwaais@users.noreply.github.com>
2025-12-04 12:43:55 +0100

    init. add RSM formula

---
+++ Formula/rsm.rb
@@ -0,0 +1,24 @@
+class Rsm < Formula
+  desc "Reference Standard M - Implementation of ANSI/MDC M X11.1-1995"
+  homepage "https://gitlab.com/Reference-Standard-M/rsm"
+  url "https://gitlab.com/Reference-Standard-M/rsm/-/archive/v1.79.0/rsm-v1.79.0.tar.gz"
+  sha256 "" # Run: curl -sL <url> | shasum -a 256
+  license "AGPL-3.0-or-later"
+  
+  head "https://gitlab.com/Reference-Standard-M/rsm.git", branch: "main"
+
+  depends_on "make" => :build
+
+  def install
+    # Build the rsm executable
+    system "make", "-j#{ENV.make_jobs}"
+    
+    # Install to Homebrew prefix
+    system "make", "install", "prefix=#{prefix}"
+  end
+
+  test do
+    # Test that rsm runs and returns version info
+    assert_match "Reference Standard M", shell_output("#{bin}/rsm -V 2>&1")
+  end
+end
